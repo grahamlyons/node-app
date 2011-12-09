65295b5a40912e3dd8a97138b99656f53f4d7ffd
Graham Lyons <graham@grahamlyons.com>
2011-12-09 08:37:25 +0000

    Updated and hopefully improved api

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var http = require('http');
+var nano = require('./lib/nano'),
+    page = '<html><head><title>gram.no.de</title></head><body><h1>gram.no.de</h1><p>Powered by <a href="http://nodejs.org/">nodejs.org</a></p></body></html>',
+    app = nano.app;
 
-var page = '<html><head><title>gram.no.de</title></head><body><h1>gram.no.de</h1><p>Powered by <a href="http://nodejs.org/">nodejs.org</a></p></body></html>';
-
-var server = http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<!DOCTYPE html>');
+app.get('/', function(request, response) {
+    response.writeHead(200, {'Content-type': 'text/html'});
+    response.write('<!DOCTYPE HTML>');
     response.end(page);
 });
 
-server.listen(process.env.PORT || 8080);
+nano.start();
