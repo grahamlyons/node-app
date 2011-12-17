ffeb6cb3ff77a926b8bf533ba4479857f376c941
Graham Lyons <graham@grahamlyons.com>
2011-12-17 14:28:16 +0000

    Added promise progress handler tests and made promise work with it

--- test/a-test.js
+++ test/a-test.js
@@ -17,13 +17,34 @@ exports.test = new litmus.Test('Test promise handling', function() {
         return p;
     }
 
-    this.plan(3);
+    function increment(start, finish, delta) {
+        var p = new Promise(),
+            i, current = start;
+
+        for(i = start; i < finish; i += delta) {
+            setTimeout(function(){
+                current += delta;
+                if(current === finish) {
+                    p.resolve(current);
+                } else if(current > finish) {
+                    p.reject(current);
+                } else {
+                    p.progress(current);
+                }
+            }, i);
+        }
+
+        return p;
+    }
+
+    this.plan(5);
 
     var test = this,
         successDesc = 'Promise resolved',
         failureDesc = 'Promise failed',
         successWithValueDesc = 'Promise resolved with value',
         failureWithValueDesc = 'Promise failed with value',
+        progressDesc = 'Promise called progress handler',
         handleSuccess = this.async('promise success', function(handle) {
             delay(100).then(
                 function() {
@@ -57,6 +78,33 @@ exports.test = new litmus.Test('Test promise handling', function() {
                 },
                 function() {
                     test.fail(successWithValueDesc);
+                    handle.resolve();
+                }
+            );
+        });
+        handleFailureWithValue = this.async('promise failure with value', function(handle) {
+            var expected = 500;
+            delay(101, expected).then(
+                function() {
+                    test.fail(failureWithValueDesc);
+                    handle.resolve();
+                },
+                function(returnVal) {
+                    test.is(returnVal, expected, failureWithValueDesc);
+                    handle.resolve();
+                }
+            );
+        })
+        handleProgress = this.async('promise progress callback', function(handle) {
+            var start = 0,
+                finish = 100,
+                expectedProgress = 50;
+            
+            increment(start, finish, 50).then(
+                function() { },
+                function() { },
+                function(progressValue) {
+                    test.is(progressValue, expectedProgress, progressDesc);
                     handle.resolve();
                 }
             );
