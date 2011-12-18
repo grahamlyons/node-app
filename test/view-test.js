f27b06f0a204e9b095ea27804f6fd5ce1a7e2e11
Graham Lyons <graham@grahamlyons.com>
2011-12-18 21:10:31 +0000

    Added more tests around setting data on view and support when promise is not returned

--- test/view-test.js
+++ test/view-test.js
@@ -15,18 +15,27 @@ exports.test = new litmus.Test('Testing the view', function() {
         filenameData = __dirname + '/test.jhtml',
         views = {};
 
+    this.plan(6);
+
     makeTemplateFile(filename, tmpl);
+    makeTemplateFile(filenameData, tmplData);
 
     views.one = new View(filename);
     this.ok(views.one instanceof View, 'Can instantiate view');
 
     this.async('render template', function(handler) {
-        views.one.render().then(
-            function(view) {
-                test.is(tmpl, view, 'Got expected text');
+        var data = views.one.render();
+
+        if(data.then) {
+            data.then(function(view) {
+                test.is(tmpl, view, 'Got expected text from promise');
                 handler.resolve();
-            }
-        );
+            });
+        }
+        else {
+            test.is(tmpl, data, 'Got expected text, but not from promise');
+            handler.resolve();
+        }
     });
 
     views.two = new View(filename);
@@ -37,15 +46,54 @@ exports.test = new litmus.Test('Testing the view', function() {
         }, 1000); //Try to leave enough time to read the file
     });
 
-    makeTemplateFile(filenameData, tmplData);
     views.three = new View(filenameData, {name: 'gram'});
     this.async('pass data in constructor', function(handler) {
-        views.three.render().then(
-            function(view) {
+        var data = views.three.render();
+
+        if(data.then) {
+            data.then(function(view) {
                 test.is('<h1>Hello gram</h1>', view, 'Got expected text with data replaced');
                 handler.resolve();
-            }
-        );
+            });
+        }
+        else {
+            test.is('<h1>Hello gram</h1>', data, 'Got expected text with data replaced, but not from promise');
+            handler.resolve();
+        }
+
+    });
+
+    views.four = new View(filenameData);
+    views.four.set('name', 'gram');
+    this.async('set data with function on instance', function(handler) {
+        var data = views.four.render();
+
+        if(data.then) {
+            data.then(function(view) {
+                test.is('<h1>Hello gram</h1>', view, 'Got expected text with data set by function');
+                handler.resolve();
+            });
+        }
+        else {
+            test.is('<h1>Hello gram</h1>', data, 'Got expected text with data set by function, but not from promise');
+            handler.resolve();
+        }
+    });
+
+    views.five = new View(filenameData);
+    this.async('set data with function on instance', function(handler) {
+        var data = views.five.render({name: 'gram'});
+
+        if(data.then) {
+            data.then(function(view) {
+                test.is('<h1>Hello gram</h1>', view, 'Got expected text with data passed to render');
+                handler.resolve();
+            });
+        }
+        else {
+            test.is('<h1>Hello gram</h1>', data, 'Got expected text with data passed to render, but not from promise');
+            handler.resolve();
+        }
     });
 
 });
