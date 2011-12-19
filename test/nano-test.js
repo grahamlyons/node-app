2e59280b76058f5b1cda1dd13a0fb6a4a08dd461
Graham Lyons <graham@grahamlyons.com>
2011-12-19 23:08:45 +0000

    Added more nano tests, probably to be changed soon

--- test/nano-test.js
+++ test/nano-test.js
@@ -10,7 +10,8 @@ function mockRequest(method, url) {
 
 exports.test = new litmus.Test('Test nano framework', function() {
 
-    var app = nano.app;
+    var app = nano.app
+        test = this;
 
     this.ok(app instanceof nano.App, 'nano.app is instance of the App object');
     this.ok(app.get, 'app has get handler');
@@ -18,10 +19,26 @@ exports.test = new litmus.Test('Test nano framework', function() {
     this.ok(app.put, 'app has put handler');
     this.ok(app.del, 'app has del handler');
 
-    app.get('/', function() {
+    app.get('/', function(request, response) {
         return 'Hello';
     });
 
-    this.is(app.dispatch(mockRequest('GET', '/'), {}), 'Hello', 'Matched route returns content define by function');
-    this.is(app.dispatch(mockRequest('HEAD', '/'), {}), 'Hello', 'Route defined for get is also served for head request');
+    this.is(
+        app.dispatch(mockRequest('GET', '/'), {}), 
+        'Hello', 
+        'Matched route returns content define by function');
+
+    this.is(
+        app.dispatch(mockRequest('HEAD', '/'), {}), 
+        'Hello', 
+        'Route defined for get is also served for head request');
+
+    app.dispatch(
+        mockRequest('GET', '/notmatched'), 
+        {writeHead:function(statusCode, headers) {
+            test.is(
+                statusCode,
+                404, 
+                'Unmatched route gives not found');
+        }, end:function() {}});
 });
--- test/suite.js
+++ test/suite.js
@@ -1,6 +1,6 @@
 var litmus = require('litmus');
 
-exports.test = new litmus.Suite('description', [
+exports.test = new litmus.Suite('Testing promises, views and framework', [
     require('./a-test.js').test,
     require('./view-test.js').test,
     require('./nano-test.js').test
